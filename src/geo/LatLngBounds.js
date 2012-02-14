0181a837d0e9fbcc48b890cc4c95508f29bebeeb
Esben Nepper <en@folia.dk>
2012-02-14 14:28:42 +0100

    Added format argument to toBBoxString in LatLngBounds.js to match Google Maps toUrlValue. format argument can be 'default' or 'google' for now.
    If no argument is given the default behavior will be used.

--- src/geo/LatLngBounds.js
+++ src/geo/LatLngBounds.js
@@ -3,19 +3,18 @@
  */
 
 L.LatLngBounds = L.Class.extend({
-	initialize: function (southWest, northEast) {	// (LatLng, LatLng) or (LatLng[])
-		if (!southWest) {
+	initialize : function(southWest, northEast) {// (LatLng, LatLng) or (LatLng[])
+		if(!southWest) {
 			return;
 		}
-		var latlngs = (southWest instanceof Array ? southWest : [southWest, northEast]);
-		for (var i = 0, len = latlngs.length; i < len; i++) {
+		var latlngs = ( southWest instanceof Array ? southWest : [southWest, northEast]);
+		for(var i = 0, len = latlngs.length; i < len; i++) {
 			this.extend(latlngs[i]);
 		}
 	},
-
 	// extend the bounds to contain the given point
-	extend: function (/*LatLng*/ latlng) {
-		if (!this._southWest && !this._northEast) {
+	extend : function(/*LatLng*/latlng) {
+		if(!this._southWest && !this._northEast) {
 			this._southWest = new L.LatLng(latlng.lat, latlng.lng, true);
 			this._northEast = new L.LatLng(latlng.lat, latlng.lng, true);
 		} else {
@@ -25,67 +24,56 @@ L.LatLngBounds = L.Class.extend({
 			this._northEast.lng = Math.max(latlng.lng, this._northEast.lng);
 		}
 	},
-
-	getCenter: function () /*-> LatLng*/ {
-		return new L.LatLng(
-				(this._southWest.lat + this._northEast.lat) / 2,
-				(this._southWest.lng + this._northEast.lng) / 2);
+	getCenter : function()/*-> LatLng*/
+	{
+		return new L.LatLng((this._southWest.lat + this._northEast.lat) / 2, (this._southWest.lng + this._northEast.lng) / 2);
 	},
-
-	getSouthWest: function () {
+	getSouthWest : function() {
 		return this._southWest;
 	},
-
-	getNorthEast: function () {
+	getNorthEast : function() {
 		return this._northEast;
 	},
-
-	getNorthWest: function () {
+	getNorthWest : function() {
 		return new L.LatLng(this._northEast.lat, this._southWest.lng, true);
 	},
-
-	getSouthEast: function () {
+	getSouthEast : function() {
 		return new L.LatLng(this._southWest.lat, this._northEast.lng, true);
 	},
+	contains : function(/*LatLngBounds or LatLng*/obj)/*-> Boolean*/
+	{
+		var sw = this._southWest, ne = this._northEast, sw2, ne2;
 
-	contains: function (/*LatLngBounds or LatLng*/ obj) /*-> Boolean*/ {
-		var sw = this._southWest,
-			ne = this._northEast,
-			sw2, ne2;
-
-		if (obj instanceof L.LatLngBounds) {
+		if( obj instanceof L.LatLngBounds) {
 			sw2 = obj.getSouthWest();
 			ne2 = obj.getNorthEast();
 		} else {
 			sw2 = ne2 = obj;
 		}
 
-		return (sw2.lat >= sw.lat) && (ne2.lat <= ne.lat) &&
-				(sw2.lng >= sw.lng) && (ne2.lng <= ne.lng);
+		return (sw2.lat >= sw.lat) && (ne2.lat <= ne.lat) && (sw2.lng >= sw.lng) && (ne2.lng <= ne.lng);
 	},
+	intersects : function(/*LatLngBounds*/bounds) {
+		var sw = this._southWest, ne = this._northEast, sw2 = bounds.getSouthWest(), ne2 = bounds.getNorthEast();
 
-	intersects: function (/*LatLngBounds*/ bounds) {
-		var sw = this._southWest,
-			ne = this._northEast,
-			sw2 = bounds.getSouthWest(),
-			ne2 = bounds.getNorthEast();
-
-		var latIntersects = (ne2.lat >= sw.lat) && (sw2.lat <= ne.lat),
-			lngIntersects = (ne2.lng >= sw.lng) && (sw2.lng <= ne.lng);
+		var latIntersects = (ne2.lat >= sw.lat) && (sw2.lat <= ne.lat), lngIntersects = (ne2.lng >= sw.lng) && (sw2.lng <= ne.lng);
 
 		return latIntersects && lngIntersects;
 	},
+	toBBoxString : function(/*output format*/format) {
+		format = format || 'default';
 
-	toBBoxString: function () {
-		var sw = this._southWest,
-			ne = this._northEast;
-		return [sw.lng, sw.lat, ne.lng, ne.lat].join(',');
-	},
-	
-	toUrlValue: function () {
-		var sw = this._southWest,
-			ne = this._northEast;
-		return [sw.lat, sw.lng, ne.lat, ne.lng].join(',');
+		var sw = this._southWest, ne = this._northEast;
+		
+		switch (format) {
+			case 'google':
+				return [sw.lat, sw.lng, ne.lat, ne.lng].join(',');
+				break;
+			case 'default':
+			default:
+				return [sw.lng, sw.lat, ne.lng, ne.lat].join(',');
+				break;
+		}
 	}
 });
 
